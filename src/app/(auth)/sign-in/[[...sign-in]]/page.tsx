e2f1ecb13b54bbe593805a406ef4271470f5bfbf
Bartosz <bartekmackowiak92@gmail.com>
2024-06-11 20:32:42 +0200

    auth

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import * as Clerk from '@clerk/elements/common';
+import { SignIn } from '@clerk/nextjs';
+// import * as SignIn from '@clerk/elements/sign-in';
+
+export default function SignInPage() {
+  return <SignIn />;
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import * as Clerk from '@clerk/elements/common';
+import { SignIn, SignUp } from '@clerk/nextjs';
+// import * as SignIn from '@clerk/elements/sign-in';
+
+export default function SignInPage() {
+  return <SignUp />;
+}
