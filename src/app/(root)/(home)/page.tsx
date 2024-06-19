61b0c82555e25d2e7699ddf668e341e9eedbc114
Bartosz <bartekmackowiak92@gmail.com>
2024-06-19 23:23:05 +0200

    message

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+import { useTheme } from '@/context/ThemeProvider';
 import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const page = () => {
+  const { mode } = useTheme();
   return (
     <div>
+      {mode}
       <UserButton />
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,27 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 
 export const metadata: Metadata = {
   title: 'Devflow',
-  description: 'Devflow is the best',
+  description:
+    'A community-driven platform for asking and answering programing questions. get help, share knowledge, and collaborate with developers from around the world explore topics in web development, mobile app development, algorithms data structures, and more',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 };
 
 export default function RootLayout({
@@ -16,9 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} ${spaceGrotesk.className}`}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
