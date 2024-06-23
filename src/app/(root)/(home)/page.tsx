c5984e0d7a7c01d77c5e40fe7eb82ce75d2c8026
Bartosz <bartekmackowiak92@gmail.com>
2024-06-23 18:21:30 +0200

    Navigation

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,16 +1,8 @@
-'use client';
 import { useTheme } from '@/context/ThemeProvider';
-import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const page = () => {
-  const { mode } = useTheme();
-  return (
-    <div>
-      {mode}
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
